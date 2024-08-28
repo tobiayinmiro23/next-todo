4855692c12f03f423a01c0d148ae6b3abf8e5410
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-08-28 12:04:25 +0100

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Icard } from '@/types'
 
-import { EditModal, Button } from '.'
+import { EditModal} from '.'
 
 
 const Card = ({ allTodos, setallTodos, getTodo }: Icard) => {
@@ -24,13 +24,13 @@ const Card = ({ allTodos, setallTodos, getTodo }: Icard) => {
             .catch(err => console.log(err))
     }
     return (
-        <div className='mt-[3rem]'>
+         <div className='mt-[3rem] max-w-[950px]'>
             {
                 allTodos?.map(data => {
-                    return <div key={data._id} className='flex items-center justify-center mb-[2rem] pl-[0.3rem] '>
-                        <h3 className=' text-center text-[1.25rem] w-[90%] m-[auto]'>{data.todo}</h3>
-                        <div className='flex items-start justify-center mx-[1rem] max-[421px]:mx-[0.3rem] max-[421px]:px-[0.3rem]'>
-                            <div onClick={() => deleteTodo(data._id)} className="w-[1.15rem] h-[2rem] "><img src="/delete.png" alt="" /></div>
+                    return <div key={data._id} className='flex items-start justify-around  mb-[2rem] pl-[0.3rem] '>
+                        <h3 className=' text-center text-[1.25rem] w-[75%] '>{data.todo}</h3>
+                        <div className='flex items-center mx-[1rem] max-[421px]:mx-[0.3rem] max-[421px]:px-[0.3rem]'>
+                            <div onClick={() => deleteTodo(data._id)} className="w-[1.15rem] cursor-pointer h-[2rem] mr-[0.8rem]"><img src="/delete.png" alt="" /></div>
                             <EditModal todo={data.todo} id={data._id} getTodo={getTodo} />
                         </div>
                     </div>
