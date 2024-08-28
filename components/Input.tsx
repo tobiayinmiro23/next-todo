531d0ba6631b8f8b2c8f70e07c4048d4970bda30
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-08-28 12:03:34 +0100

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,8 +5,8 @@ import { Button } from '.'
 const Input = ({ settodoText, todoText, addTodo }: input) => {
 
     return (
-        <div className='my-[2rem] w-[100%] m-[auto] flex-center ' >
-            <input value={todoText} onChange={(e) => settodoText(e.target.value)} className='bg-[#f5f2f2] w-[60%] border-[#e8e5e4] border-[1px] py-[0.7rem] px-[1rem] rounded-full outline-none max-[452px]:w-[65%] max-[452px]:m-[auto]  max-[452px]:mx-[0.3rem] max-[308px]:w-[61%]' type="text" placeholder='add a todo to keep track of your tasks' />
+        <div className='my-[2rem] w-[100%] m-[auto] flex items-center justify-center ' >
+            <input value={todoText} onChange={(e) => settodoText(e.target.value)} className='bg-[#f5f2f2] w-[70%] border-[#e8e5e4] border-[1px] py-[0.7rem] px-[1rem] rounded-full outline-none max-[452px]:w-[65%] max-[452px]:m-[auto]  max-[452px]:mx-[0.3rem] max-[308px]:w-[61%]' type="text" placeholder='add a todo to keep track of your tasks' />
             <Button style='bg-[orange] m-[1rem] rounded-full py-[0.65rem] px-[1rem] shadow-md text-[#ffffff]  max-[452px]:mx-[0.3rem]  max-[386px]:px-[0.6rem]' handleButton={addTodo} text='Add Todo' />
         </div>
 
